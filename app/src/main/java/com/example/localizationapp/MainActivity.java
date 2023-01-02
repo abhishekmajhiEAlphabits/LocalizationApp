dad92339cefaab43e7a19e376fa50911d888f9e5
Abhishek Majhi <abhishek@ealphabits.com>
2023-01-02 16:42:17 +0530

    First Commit

---
+++ app/src/main/java/com/example/localizationapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.localizationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Locale locale = new Locale("fr");
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+    }
+}
